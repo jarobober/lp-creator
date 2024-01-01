90fed5eece1f0e9715e5ce1d5fc32762082ec9e5
Jarek Bober <jbober@user.com>
2024-01-01 01:55:23 +0100

    init grapes

---
+++ src/views/GrapesView.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import grapesjs from "grapesjs";
+import "grapesjs/dist/css/grapes.min.css";
+
+const grapesCanvasRef = ref<HTMLElement>();
+
+onMounted(() => {
+  grapesjs.init({
+    container: grapesCanvasRef.value,
+    fromElement: true,
+    storageManager: {
+      type: "local",
+      autosave: true,
+      autoload: true,
+    },
+    canvas: {
+      styles: [],
+      scripts: [],
+    },
+  });
+});
+</script>
+
+<template>
+  <div ref="grapesCanvasRef"></div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
-<script lang="ts" setup>
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="text-h2 font-weight-bold">
+          Landing page creator initial site
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn color="primary" :to="{ name: 'Creator' }">Go to creator</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
